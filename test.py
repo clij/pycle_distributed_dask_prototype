ede137133fc6e6d1fc71182df6405842b7ea07d5
JonathanCSmith <jon@jonathansmith.me>
2022-10-07 14:36:25 +0100

    Add test entrant

---
+++ test.py
@@ -0,0 +1,5 @@
+from tests.slurm_cluster_tests import run_connected_components_test, run_guassian_filter_test
+
+if __name__ == '__main__':
+    run_connected_components_test()
+    run_guassian_filter_test()
